277187df0b06085a56416fcb10c4dcad5d8dba2d
김태현 <bear2u@naver.com>
2019-02-26 20:14:51 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/pe/kr/udemy_listview_clone/MainActivity.kt
@@ -0,0 +1,13 @@
+package pe.kr.udemy_listview_clone
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
